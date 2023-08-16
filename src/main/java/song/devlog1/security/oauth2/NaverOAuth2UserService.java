f508cfcdd4f5bedd0c5d1593d615da97368a86c0
song <dkclasltmf@naver.com>
2023-08-16 19:48:25 +0900

    1

--- src/main/java/song/devlog1/security/oauth2/NaverOAuth2UserService.java
+++ src/main/java/song/devlog1/security/oauth2/NaverOAuth2UserService.java
@@ -17,40 +17,40 @@ import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
-//@Service
-//@RequiredArgsConstructor
-//public class NaverOAuth2UserService extends DefaultOAuth2UserService {
-public class NaverOAuth2UserService {
+@Service
+@RequiredArgsConstructor
+public class NaverOAuth2UserService extends DefaultOAuth2UserService {
+//public class NaverOAuth2UserService {
 
-//    private final UserJpaRepository userRepository;
+    private final UserJpaRepository userRepository;
 
-//    @Override
-//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//        OAuth2User oAuth2User = super.loadUser(userRequest);
-//        UserInfo userInfo = null;
-//
-//        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-//
-//        if ("naver".equals(registrationId)) {
-//            userInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
-//        } else {
-//            log.info("OAuth 로그인 실패");
-//        }
-//
-//        String provider = userInfo.getProvider();
-//        String providerId = userInfo.getProviderId();
-//        String username = provider + "_" + providerId;
-//
-//        String sex = userInfo.getSex();
-//        String role = RoleName.ROLE_USER.name();
-//        log.info("{}", oAuth2User.getAttributes());
-//
-//        User user = null;
-//        Optional<User> findUser = userRepository.findByUsername(username);
-//        if (findUser.isEmpty()) {
-////            user=
-//        }
-//
-//        return null;
-//    }
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        UserInfo userInfo = null;
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        if ("naver".equals(registrationId)) {
+            userInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
+        } else {
+            log.info("OAuth 로그인 실패");
+        }
+
+        String provider = userInfo.getProvider();
+        String providerId = userInfo.getProviderId();
+        String username = provider + "_" + providerId;
+
+        String sex = userInfo.getSex();
+        String role = RoleName.ROLE_USER.name();
+        log.info("{}", oAuth2User.getAttributes());
+
+        User user = null;
+        Optional<User> findUser = userRepository.findByUsername(username);
+        if (findUser.isEmpty()) {
+//            user=
+        }
+
+        return null;
+    }
 }
