ccce3958159fd54fb9f277f721807061f2b4b2fb
song <dkclasltmf@naver.com>
2023-08-17 14:52:03 +0900

    1

--- src/main/java/song/devlog1/WebConfig.java
+++ src/main/java/song/devlog1/WebConfig.java
@@ -10,11 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://http://52.79.222.161/:8080",
-                        "http://localhost:3000",
-                        "https://localhost:3000",
-                        "https://127.0.0.1:3000"
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
         ;
 
