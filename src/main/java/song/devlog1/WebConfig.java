18996a3342308195bd3671464789ea88ca444b30
song <dkclasltmf@naver.com>
2023-08-24 22:57:00 +0900

    e

--- src/main/java/song/devlog1/WebConfig.java
+++ src/main/java/song/devlog1/WebConfig.java
@@ -3,7 +3,6 @@ package song.devlog1;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,14 +17,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600);
     }
-
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/{spring:\\w+}")
-//                .setViewName("forward:/");
-//        registry.addViewController("/**/{spring:\\w+}")
-//                .setViewName("forward:/");
-//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-//                .setViewName("forward:/");
-//    }
 }
